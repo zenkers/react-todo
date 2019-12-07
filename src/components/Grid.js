e36270bff58dc4aad2b923b7ef432588125e1ba5
Zenkers <dpswanson2010@gmail.com>
2019-12-07 09:42:18 -0500

    Adjusted structure of /src

---
+++ src/components/Grid.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GridItem from './GridItem';
+
+function Grid() {
+  return (
+    <div className="grid">
+      Test
+      <GridItem></GridItem>
+    </div>
+  );
+}
+export default Grid;
---
+++ src/components/GridItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function gridItem() {
+  return (
+    <div className="grid-item">
+      Test2
+    </div>
+  );
+}
+
+export default gridItem;
